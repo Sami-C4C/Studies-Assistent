5df3509913a0d97ef1e4c40bbce8fb7700e6a0ee
Sami-C4C <101250244+Sami-C4C@users.noreply.github.com>
2025-01-31 11:18:45 +0100

    Update settings.py
    Secret_Key removed.

--- StudiesAssistant/settings.py
+++ StudiesAssistant/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r!3=^j5h9l+i^8iz$gueoos!905am5q53ah$qdxdi4=6j78zy)'
+SECRET_KEY = 'Use here your own secret_Key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
